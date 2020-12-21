5865fa4d089e3a6acda9050c6b0db8391c8320cf
Emer Thornbury <EThornbury@staff.ncirl.ie>
2020-12-21 10:06:48 +0000

    welcome controller, root added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'welcome#home'
+  #get 'welcome/home'
+  get 'welcome/about'
   resources :items
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
